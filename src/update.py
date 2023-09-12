a17b2f1789e3e49e308adf0c0f0499122d147ab4
MarvinFenske <81228640+MarvinFenske@users.noreply.github.com>
2023-09-12 21:02:11 +0200

    Update update.py
    User and pw now in .env

--- src/update.py
+++ src/update.py
@@ -1,6 +1,10 @@
 import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def updateInsert (collection, json):
-  client = pymongo.MongoClient('mongodb+srv://marvin:9jZ05DgIh6qNeMrp@mongodbcluster.n6cun7v.mongodb.net/')
+  client = pymongo.MongoClient('mongodb+srv://' + os.getenv('MongoUser') + ':' + os.getenv('MongoPassword') + '@mongodbcluster.n6cun7v.mongodb.net/')
   db = client['MongoDB-Database']
   col = db[collection]
 
