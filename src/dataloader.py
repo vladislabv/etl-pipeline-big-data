d707229ae9f3e74b0923d04b0ff88b5bd8481669
Hendimetz <HendrikMetzner@gmail.com>
2023-09-14 16:18:51 +0200

    Insert into MongoDB
    Die Daten aufbereitet das sie den Feldern in Mongo DB entsprechen und FUnktionsaufruf zum speichern in die MongoDB erstellt

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,8 +2,10 @@ import os
 import requests
 #import pymongo
 from dotenv import load_dotenv
-
+from datetime import datetime, timedelta, timezone
 from api import check_api_is_up, get_gateways, get_tags, get_measurements, get_configs, get_configssing
+from update import updateInsert, closeclient
+
 
 load_dotenv()  # take environment variables from .env.
 
@@ -14,6 +16,9 @@ HEADERS = {'accept': 'application/json'}
 with requests.Session() as s:
     # set headers
     numbermeasurements = 0
+    numbergateways = 0
+    numbertags = 0
+
     s.headers.update(HEADERS)
     print(f'Checking if API is up at {HOST}')
     if check_api_is_up(s):
@@ -21,25 +26,53 @@ with requests.Session() as s:
         print('Getting gateways')
     
         for gateway in get_gateways(s):
+            numbergateways += 1
             print(f'Gateway: {gateway["id"]}\n')
             print('Getting tags')
 
             gatewayconfig = get_configssing(s, gateway["id"])
+            
+            gateway['ip4'] = gateway.pop('ip_address')
+            gateway['net_segment'] = gateway.pop('network_segment')
+            gateway['_id'] = gateway.pop('id')
+            gateway['last_contact'] = datetime.strptime(gateway['last_contact'][:-4], "%Y-%m-%dT%H:%M:%S.%f").replace(tzinfo=timezone.utc)
+
+            gateway['config'] = gatewayconfig
+            gateway['tags_assigned'] = []
+
             print(f'Config for gateway: {gatewayconfig}\n')
 
-            for tag in get_tags(s, gateway['id']):
+            for tag in get_tags(s, gateway['_id']):
+                numbertags += 1
                 print(f'Tag: {tag["address"]}\n')
-                
-                tagconfig = get_configssing(s, gateway["id"], tag['address'])
+
+                tag['_id'] = tag.pop('address')
+                tag['last_contact'] = datetime.strptime(tag['last_contact'][:-4], "%Y-%m-%dT%H:%M:%S.%f").replace(tzinfo=timezone.utc)
+                tag.pop('sensors')
+
+                tagconfig = get_configssing(s, gateway["_id"], tag['_id'])
                 print(f'Config for tag: {tagconfig}\n')
 
-                print('Getting measurements')
+                tag['config'] = tagconfig
+            
+                gateway['tags_assigned'].append(tag['_id'])
                 
-                for measurement in get_measurements(s, tag['address']):
+                updateInsert('tags', tag)
+
+                print('Getting measurements')
+
+                for measurement in get_measurements(s, tag['_id']):
                     numbermeasurements += 1
+
+                    measurement['tag_id'] = tag['_id']
                     print(f'Measurement: {measurement}\n')
-                    
-        print("Everything loaded with " + str(numbermeasurements) + " measurements")
+            
+
+
+            updateInsert('gateways', gateway)
+
+        closeclient            
+        print("Everything loaded with " + str(numbermeasurements) + " measurements, " + str(numbertags) + " tags und  " + str(numbergateways) + " gateways ")
     else:    
         print('API is not up. Program will be closed')
 
--- src/update.py
+++ src/update.py
@@ -1,17 +1,19 @@
+import os
 import pymongo
 from dotenv import load_dotenv
 
 load_dotenv()
-
+client = pymongo.MongoClient('mongodb+srv://' + os.getenv('MongoUser') + ':' + os.getenv('MongoPassword') + '@mongodbcluster.n6cun7v.mongodb.net/')
+db = client['MongoDB-Database']
+  
 def updateInsert (collection, json):
-  client = pymongo.MongoClient('mongodb+srv://' + os.getenv('MongoUser') + ':' + os.getenv('MongoPassword') + '@mongodbcluster.n6cun7v.mongodb.net/')
-  db = client['MongoDB-Database']
   col = db[collection]
 
   query = {'_id': json['_id']}
   value = {'$set': json}
 
   x = col.update_one(query, value, upsert = True)
-
-  client.close();
   return x
+
+def closeclient() :
+  client.close();
